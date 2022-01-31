ccb5e700ab6c2acc646a832ef2861d14bf94c60e
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-01-31 04:02:47 -0500

    game logic and improved display

--- script.js
+++ script.js
@@ -1,51 +1,162 @@
 const size = 9;
 const bombs = 10;
+const totalGuesses = 10;
+
+var numSelected = 0;
+var revealedBombs = 0;
+var turnNumber = 0;
 
 /**
  * represents the board
  * 0 = empty
  * 1 = guess
- * 2 = bomb
- * 3 = revealed
+ * 2 = revealed
  */
-var board = Array(size).fill(0).map(x => Array(size).fill(0))
+var boardGuesses = Array(size).fill(0).map(x => Array(size).fill(0))
 
-function boardClick(e, row, col) {
-    if (e.isClicked) {
-        e.style.backgroundColor = '#dddddd';
-        board[row][col] = 0;
-    } else {
-        e.style.backgroundColor = '#00ff00';
-        board[row][col] = 1;
-    }
-    e.isClicked = !e.isClicked;
+/**
+ * 1 = bomb, 0 = empty
+ */
+var board = Array(size).fill(0).map(x => Array(size).fill(0));
+var cells = document.getElementsByClassName('cell');
+
+window.onload = () => {
+    generateBoard();
+
+    document.getElementById('numSelected').innerHTML = numSelected;
+    document.getElementById('totalGuesses').innerHTML = totalGuesses;
+
+    document.getElementById('revealedBombs').innerHTML = revealedBombs;
+    document.getElementById('bombs').innerHTML = bombs;
 }
 
-function guess() {
-    var numGuesses = 0;
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
-            if (board[i][j] == 1) {
-                console.log(i, j);
-                numGuesses++;
+function generateBoard() {
+    var count = 0;
+    while (count < bombs) {
+        var row = Math.floor(Math.random() * size);
+        var col = Math.floor(Math.random() * size);
+        if (board[row][col] != 1) {
+            board[row][col] = 1;
+            count++;
+        }
+    }
+}
+
+function revealSquares(row, col) {
+    // check bounds on row and col
+    if (row < 0 || row >= size || col < 0 || col >= size) return;
+
+    document.getElementById(`${row}-${col}`).revealed = true;
+
+    // if the square is a bomb
+    if (board[row][col] === 1) {
+        revealedBombs++;
+        document.getElementById(`${row}-${col}`).style.backgroundColor = '#000000';
+        return;
+    }
+
+    document.getElementById(`${row}-${col}`).style.backgroundColor = "#ffffff";
+    var numAdjacentBombs = 0;
+    if (row > 0) if (board[row - 1][col] === 1) numAdjacentBombs++;
+    if (col > 0) if (board[row][col - 1] === 1) numAdjacentBombs++;
+    if (row < size - 1) if (board[row + 1][col] === 1) numAdjacentBombs++;
+    if (col < size - 1) if (board[row][col + 1] === 1) numAdjacentBombs++;
+
+    if (numAdjacentBombs > 0) {
+        document.getElementById(`${row}-${col}`).innerHTML = numAdjacentBombs;
+        return;
+    } else {
+        if (row > 0) {
+            if (!document.getElementById(`${row - 1}-${col}`).revealed) {
+                revealSquares(row - 1, col);
+            }
+        }
+        if (col > 0) {
+            if (!document.getElementById(`${row}-${col - 1}`).revealed) {
+                revealSquares(row, col - 1);
+            }
+        }
+        if (row < size - 1) {
+            if (!document.getElementById(`${row + 1}-${col}`).revealed) {
+                revealSquares(row + 1, col);
+            }
+        }
+        if (col < size - 1) {
+            if (!document.getElementById(`${row}-${col + 1}`).revealed) {
+                revealSquares(row, col + 1);
             }
         }
     }
-    if (numGuesses != bombs) {
-        window.alert('Make sure to guess ' + (bombs - numGuesses) + ' more bombs');
+}
+
+function boardClick(e, row, col) {
+    if (e.revealed) return;
+
+    if (e.clicked) {
+        e.style.backgroundColor = '#dddddd';
+        boardGuesses[row][col] = 0;
+        numSelected--;
+    } else {
+        if (numSelected >= 10) return;
+
+        e.style.backgroundColor = '#00ff00';
+        boardGuesses[row][col] = 1;
+        numSelected++;
+    }
+    e.clicked = !e.clicked;
+
+    document.getElementById('numSelected').innerHTML = numSelected;
+}
+
+function guess() {
+    if (numSelected != totalGuesses - revealedBombs) {
+        window.alert('Make sure to guess ' + (totalGuesses - revealedBombs - numSelected) + ' more bombs');
+        return;
+    }
+
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            if (boardGuesses[i][j] === 1) {
+                boardGuesses[i][j] = 2;
+                revealSquares(i, j);
+            }
+        }
+    }
+
+    numSelected = 0;
+    document.getElementById('numSelected').innerHTML = 0;
+    document.getElementById('totalGuesses').innerHTML = totalGuesses - revealedBombs;
+
+    document.getElementById('revealedBombs').innerHTML = revealedBombs;
+
+    turnNumber++;
+    document.getElementById('turnNumber').innerHTML = turnNumber + ' guess' + (turnNumber == 1 ? '' : 'es');
+
+    if (revealedBombs == bombs) {
+        window.alert('You win!');
     }
 }
 
 function clearGuess() {
     var allCells = document.getElementsByClassName('cell');
     for (let e of allCells) {
+        if (e.revealed) continue;
         e.style.backgroundColor = '#dddddd';
-        e.isClicked = false;
+        e.clicked = false;
     }
+}
 
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
-            board[i][j] = 0;
-        }
+function reset() {
+    board = Array(size).fill(0).map(x => Array(size).fill(0));
+    boardGuesses = Array(size).fill(0).map(x => Array(size).fill(0));
+    revealedBombs = 0;
+    generateBoard();
+
+    var allCells = document.getElementsByClassName('cell');
+    for (let e of allCells) {
+        e.revealed = false;
+        e.style.backgroundColor = '#dddddd';
+        e.clicked = false;
+        e.innerHTML = '';
     }
 }
