8b77d5ea6e658bfb51037833b9e887e664f41d8d
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-01-31 21:24:24 -0500

    multiple sizes, select multiple squares at once, and don't need to use all guesses at once

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-const size = 9;
-const bombs = 10;
-const totalGuesses = 10;
+const boardPixelWidth = 500;
+
+const possibleSizes = [9, 16, 30];
+const possibleBombs = [10, 40, 160];
+
+var currentDifficulty = 0;
+
+/**
+ * The number of squares in one side of the board.
+ */
+var size = possibleSizes[currentDifficulty];
+var numBombs = possibleBombs[currentDifficulty];
+var totalGuesses = numBombs;
 
 var numSelected = 0;
 var revealedBombs = 0;
+var squaresUsed = 0;
 var turnNumber = 0;
 
 /**
- * represents the board
- * 0 = empty
- * 1 = guess
- * 2 = revealed
+ * Represents the board as a 2d array:
+ * true = the square is selected, false = the square is not selected.
  */
-var boardGuesses = Array(size).fill(0).map(x => Array(size).fill(0))
+var boardSelection = Array(size).fill(false).map(x => Array(size).fill(false));
 
 /**
- * 1 = bomb, 0 = empty
+ * 2d array of bombs, where true (1) = bomb and false (0) = no bomb.
  */
-var board = Array(size).fill(0).map(x => Array(size).fill(0));
-var cells = document.getElementsByClassName('cell');
+var hasBomb = Array(size).fill(false).map(x => Array(size).fill(false));
 
 window.onload = () => {
     generateBoard();
     generateMines();
 }
+
+var selectMultipleCells = false;
+window.addEventListener('keydown', (event) => {
+    selectMultipleCells = (event.key === 'Control');
+});
+
+window.addEventListener('keyup', (event) => {
+    selectMultipleCells = false;
+});
 
 function generateBoard() {
     for (let i = 0; i < size; i++) {
@@ -34,19 +51,27 @@ function generateBoard() {
             let cell = document.createElement('div');
             cell.className = 'cell';
             cell.id = `${i}-${j}`;
-            cell.style.width = `${450/size}px`;
-            cell.style.height = `${450/size}px`;
-            cell.style.lineHeight = `${450/size}px`;
-            cell.style.fontSize = `${0.4 * 450/size}px`;
+            cell.style.width = `${boardPixelWidth/size}px`;
+            cell.style.height = `${boardPixelWidth/size}px`;
+            cell.style.lineHeight = `${boardPixelWidth/size}px`;
+            cell.style.fontSize = `${0.4 * boardPixelWidth/size}px`;
 
             cell.clicked = false;
             cell.revealed = false;
 
-            cell.onclick = () => {
-                boardClick(cell, i, j);
+            cell.onclick = (event) => {
+                boardClick(cell, i, j, false);
             }
-            cell.oncontextmenu = (e) => {
-                e.preventDefault();
+
+            cell.onmouseover = (event) => {
+                if (selectMultipleCells) {
+                    boardClick(cell, i, j, true);
+                }
+            }
+
+
+            cell.oncontextmenu = (event) => {
+                event.preventDefault();
             }
             row.appendChild(cell);
         }
@@ -57,14 +82,28 @@ function generateBoard() {
 
 function generateMines() {
     var count = 0;
-    while (count < bombs) {
+
+    while (count < numBombs) {
         var row = Math.floor(Math.random() * size);
         var col = Math.floor(Math.random() * size);
-        if (board[row][col] != 1) {
-            board[row][col] = 1;
+        if (!hasBomb[row][col]) {
+            hasBomb[row][col] = true;
             count++;
         }
     }
+}
+
+function changeSize() {
+    currentDifficulty++;
+    if (currentDifficulty >= possibleSizes.length) currentDifficulty = 0;
+
+    size = possibleSizes[currentDifficulty];
+    numBombs = possibleBombs[currentDifficulty];
+    totalGuesses = numBombs;
+
+    document.getElementById('board').innerHTML = '';
+    generateBoard();
+    reset();
 }
 
 function revealSquares(row, col) {
@@ -74,7 +113,7 @@ function revealSquares(row, col) {
     document.getElementById(`${row}-${col}`).revealed = true;
 
     // if the square is a bomb
-    if (board[row][col] === 1) {
+    if (hasBomb[row][col]) {
         revealedBombs++;
         document.getElementById(`${row}-${col}`).style.backgroundColor = '#000000';
         return;
@@ -82,15 +121,15 @@ function revealSquares(row, col) {
 
     document.getElementById(`${row}-${col}`).style.backgroundColor = "#ffffff";
     var numAdjacentBombs = 0;
-    if (row > 0) if (board[row - 1][col] === 1) numAdjacentBombs++;
-    if (col > 0) if (board[row][col - 1] === 1) numAdjacentBombs++;
-    if (row < size - 1) if (board[row + 1][col] === 1) numAdjacentBombs++;
-    if (col < size - 1) if (board[row][col + 1] === 1) numAdjacentBombs++;
+    if (row > 0) if (hasBomb[row - 1][col]) numAdjacentBombs++;
+    if (col > 0) if (hasBomb[row][col - 1]) numAdjacentBombs++;
+    if (row < size - 1) if (hasBomb[row + 1][col]) numAdjacentBombs++;
+    if (col < size - 1) if (hasBomb[row][col + 1]) numAdjacentBombs++;
 
-    if (row > 0 && col > 0) if (board[row - 1][col - 1] === 1) numAdjacentBombs++;
-    if (row < size - 1 && col > 0) if (board[row + 1][col - 1] === 1) numAdjacentBombs++;
-    if (row > 0 && col < size - 1) if (board[row - 1][col + 1] === 1) numAdjacentBombs++;
-    if (row < size - 1 && col < size - 1) if (board[row + 1][col + 1] === 1) numAdjacentBombs++;
+    if (row > 0 && col > 0) if (hasBomb[row - 1][col - 1]) numAdjacentBombs++;
+    if (row < size - 1 && col > 0) if (hasBomb[row + 1][col - 1]) numAdjacentBombs++;
+    if (row > 0 && col < size - 1) if (hasBomb[row - 1][col + 1]) numAdjacentBombs++;
+    if (row < size - 1 && col < size - 1) if (hasBomb[row + 1][col + 1]) numAdjacentBombs++;
 
     if (numAdjacentBombs > 0) {
         document.getElementById(`${row}-${col}`).innerHTML = numAdjacentBombs;
@@ -139,18 +178,20 @@ function revealSquares(row, col) {
     }
 }
 
-function boardClick(e, row, col) {
+function boardClick(e, row, col, forceClick) {
     if (e.revealed) return;
 
     if (e.clicked) {
+        if (forceClick) return;
+
         e.style.backgroundColor = '#dddddd';
-        boardGuesses[row][col] = 0;
+        boardSelection[row][col] = false;
         numSelected--;
     } else {
         if (numSelected >= totalGuesses - revealedBombs) return;
 
         e.style.backgroundColor = '#00ff00';
-        boardGuesses[row][col] = 1;
+        boardSelection[row][col] = true;
         numSelected++;
     }
     e.clicked = !e.clicked;
@@ -159,15 +200,13 @@ function boardClick(e, row, col) {
 }
 
 function guess() {
-    if (numSelected != totalGuesses - revealedBombs) {
-        window.alert('Make sure to guess ' + (totalGuesses - revealedBombs - numSelected) + ' more bombs');
-        return;
-    }
+    squaresUsed += numSelected;
+    document.getElementById('squaresUsed').innerHTML = squaresUsed;
 
     for (var i = 0; i < size; i++) {
         for (var j = 0; j < size; j++) {
-            if (boardGuesses[i][j] === 1) {
-                boardGuesses[i][j] = 2;
+            if (boardSelection[i][j]) {
+                boardSelection[i][j] = false;
                 revealSquares(i, j);
             }
         }
@@ -182,7 +221,7 @@ function guess() {
     turnNumber++;
     document.getElementById('turnNumber').innerHTML = turnNumber + ' guess' + (turnNumber == 1 ? '' : 'es');
 
-    if (revealedBombs == bombs) {
+    if (revealedBombs == numBombs) {
         window.alert('You win!');
     }
 }
@@ -200,15 +239,21 @@ function clearGuess() {
 }
 
 function reset() {
-    board = Array(size).fill(0).map(x => Array(size).fill(0));
-    boardGuesses = Array(size).fill(0).map(x => Array(size).fill(0));
+    hasBomb = Array(size).fill(false).map(x => Array(size).fill(false));
+    boardSelection = Array(size).fill(false).map(x => Array(size).fill(false));
+    
     numSelected = 0;
     revealedBombs = 0;
+    squaresUsed = 0;
     turnNumber = 0;
+
     document.getElementById('numSelected').innerHTML = 0;
     document.getElementById('totalGuesses').innerHTML = totalGuesses;
     document.getElementById('revealedBombs').innerHTML = 0;
     document.getElementById('turnNumber').innerHTML = '0 guesses';
+    document.getElementById('squaresUsed').innerHTML = 0;
+
+    document.getElementById('bombs').innerHTML = numBombs;
 
     generateMines();
 
